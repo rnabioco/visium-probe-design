9c1de8f50d7c607af811ae5fef0eb0d2f0b3a2aa
Jay Hesselberth <jay.hesselberth@cuanschutz.edu>
2023-02-05 07:34:19 -0700

    working impl

---
+++ README.md
@@ -0,0 +1,20 @@
+# visium-design-probes.py
+
+Design probe pairs for the Visium Fixed RNA assay.
+
+Input is a FASTA file containing target sequences. Assording to the
+design guidelines, these ideally should be coding regions, not UTRs.
+
+In addition, input sequences should be pre-screened for repeat elements.
+
+To generate a sample list of probes:
+
+```bash
+python src/visium-design-probes.py test/test.fa
+```
+
+To run the tests:
+
+```bash
+python -m doctest src/visium-design-probes.py
+```
--- src/visium-probe-design.py
+++ src/visium-probe-design.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#GGGGGGGGG! /usr/bin/env python3
 
 '''
 visium--probe-design.py
@@ -13,13 +13,12 @@ A PDF from 10x with design guidelines is in `/doc`.
 __author__ = 'Jay Hesselberth <jay.hesselberth@cuanschutz.edu>'
 __licence__ = 'MIT'
 
+import pdb
+
 from collections import Counter, defaultdict
-from itertools import combinations
 
 import click
 import rle
-
-import pdb
 
 from Bio import SeqIO
 
@@ -28,21 +27,22 @@ PROBE_RHS = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 MOD_RHS   = '/5Phos/-'
 
 PROBE_LEN = 25
-GC_MAX    = 73
-GC_MIN    = 44
-HOMOP_MAX = 4
+GC_MAX    = 0.73
+GC_MIN    = 0.44
+HOMOP_MAX = 3
+
+ALLOWED_NUCS = set(['A','G','T','C']) 
 
 @click.command()
 @click.argument('target_fasta')
-def main():
+def main(target_fasta):
     cand_probes = defaultdict(list)
 
-    for record in SeqIO.parse(open(target_fasta)):
-        for pos, target_seq in gen_subseq(record.seq):
+    for record in SeqIO.parse(open(target_fasta), format = 'fasta'):
+        rc_seq = record.seq.reverse_complement()
 
-            lhs = target_seq[:PROBE_LEN]
-            rhs = target_seq[-PROBE_LEN:]
-                
+        for pos, lhs, rhs in gen_probe_pairs(rc_seq):
+
             if not _check_lhs_seq(lhs):
                 continue
             if not all(map(_check_gc, [lhs, rhs])):
@@ -50,12 +50,12 @@ def main():
             if not all(map(_check_homopolymer, [lhs, rhs])):
                 continue
 
-            cand_pairs[record.id].append(ProbePair(lhs, rhs, pos))
+            cand_probes[record.id].append(ProbePair(lhs, rhs, pos))
 
+    keep_probes = defaultdict(list)
     for id, probes in cand_probes.items():
-        last_pos = None
-        keep_probes = defaultdict(list)
 
+        last_pos = None
         for probe in probes:
             if not last_pos:
                 last_pos = probe.lhs_start
@@ -63,53 +63,95 @@ def main():
 
             if abs(probe.lhs_start - last_pos) < (PROBE_LEN * 2): continue
 
-    colnames = ['#id', 'hyb_lhs', 'hyb_rhs', 'probe_lhs', 'probe_rhs']
+            keep_probes[id].append(probe)
+            last_pos = probe.lhs_start
+
+    colnames = ['#id', 'pos', 'hyb_lhs', 'hyb_rhs', 'probe_lhs', 'probe_rhs']
     print('\t'.join(colnames))
 
-    for id, probes in keep_probes:
-        for lhs, rhs in probes:
-            fields = [id, lhs, rhs,
-                      PROBE_LHS + lhs,
-                      MOD_RHS + rhs + PROBE_RHS]        
-            print('\t'.join(fields))
+    for id, probes in keep_probes.items():
+        for probe in probes:
+            fields = [id, probe.lhs_start,
+                      probe.lhs, probe.rhs,
+                      PROBE_LHS + probe.lhs,
+                      MOD_RHS + probe.rhs + PROBE_RHS]        
+            print('\t'.join(map(str, fields)))
 
 class ProbePair():
-    def __init__(lhs, rhs, pos):
+    def __init__(self, lhs, rhs, pos):
         self.lhs = lhs
         self.rhs = rhs
         # coordinate of the start of the lhs on the reference
         self.lhs_start = pos
 
-def gen_subseq(seq):
-    for i in len(seq):
+def gen_probe_pairs(seq):
+
+    for i in range(len(seq)):
         subseq = seq[i:i+(PROBE_LEN * 2)]
         if len(subseq) < PROBE_LEN * 2: break
 
-        yield (i, subseq)
+        lhs = subseq[:PROBE_LEN]
+        rhs = subseq[-PROBE_LEN:]
+                
+        yield (i, lhs, rhs)
 
 def _check_gc(seq):
-    ''' min / max come from 10x spec sheet '''
+    '''
+    min / max come from 10x spec sheet
+
+    >>> seq1 = 'GGGGGGGGG'
+    >>> _check_gc(seq1)
+    False
+
+    >>> seq2 = 'GGGGGGGGGGAAAAAAAAAAA'
+    >>> _check_gc(seq2)
+    True
+
+    >>> seq3 = 'NNNNN'
+    >>> _check_gc(seq3)
+    False
+    '''
     ct = Counter(seq)
-    gc = ct['G'] + ct['C'] / ct.total()
+    if not all([i in ALLOWED_NUCS for i in ct.keys()]):
+        return False
+
+    gc = (ct['G'] + ct['C']) / ct.total()
     return gc >= GC_MIN and gc <= GC_MAX
 
 def _check_homopolymer(seq):
-    ''' use rle to check homopolymer stretches.
-        return: True if any stretches greater then max_len
+    '''
+    use rle to check homopolymer stretches.
+    return: True if any stretches greater then max_len
+
+    >>> seq1 = 'AAAATTTTCCCCGGGG'
+    >>> _check_homopolymer(seq1)
+    False
+
+    >>> seq2 = 'ATGCTAGCTAGTCGAT'
+    >>> _check_homopolymer(seq2)
+    True
     '''
     hps = rle.encode(seq)
-    return any([i > HOMOP_MAX for i in hps[1]])
+    return not any([i > HOMOP_MAX for i in hps[1]])
 
 def _check_lhs_seq(seq):
-    ''' At the ligation junction (where the two probes
+    '''
+    At the ligation junction (where the two probes
     meet), the 3' end of the LHS probe should be a
-    thymine.'''
+    thymine.
+
+    >>> seq1 = 'ATGTT'
+    >>> _check_lhs_seq(seq1)
+    True
+
+    >>> seq1 = 'ATGTC'
+    >>> _check_lhs_seq(seq1)
+    False
+    '''
     if seq[-1] == 'T':
         return True
     return False
 
-def check_overlaps(seq):
-    pass
-
 if __name__ == '__main__':
     main()
+
